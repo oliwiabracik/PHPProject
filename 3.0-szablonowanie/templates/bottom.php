b0ae693b2cde43ff9a1fa2c6665fa140e3953b2c
oliwiabracik <116725527+oliwiabracik@users.noreply.github.com>
2024-11-08 14:45:11 +0100

    Add files via upload

---
+++ 3.0-szablonowanie/templates/bottom.php
@@ -0,0 +1,12 @@
+</div><!-- content -->
+
+<div class="footer">
+	<p>
+		<?php out($page_footer); if (!isset($page_footer)) {  ?> Domyślna zawartość stopki ... <?php } ?>
+	</p>
+	<p>
+	</p>
+</div>
+
+</body>
+</html>
